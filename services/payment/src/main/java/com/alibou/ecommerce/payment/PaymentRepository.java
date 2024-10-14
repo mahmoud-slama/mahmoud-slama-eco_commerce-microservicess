08a77b223f41994c67b216a405c37ba3d2fe61a3
mahmoud slama <117111552+mahmoud-slama@users.noreply.github.com>
2024-10-14 21:23:04 +0200

    Commit sans le dossier getway

---
+++ services/payment/src/main/java/com/alibou/ecommerce/payment/PaymentRepository.java
@@ -0,0 +1,5 @@
+package com.alibou.ecommerce.payment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+}
